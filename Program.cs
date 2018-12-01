b9adaf88f8bea4f36891dbf35417479fe7d2a1ce
brian zab <brian.zabandith@epitech.eu>
2018-12-01 19:22:03 +0100

    first commit of the proj

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WorkTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tmp = new StringTry("azbnsdflkopaizepoe");
+
+            tmp.CountDistinct();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StringTry.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WorkTest
+{
+    public class StringTry
+    {
+        private string _text;
+
+        public StringTry(string text)
+        {
+            _text = text;
+        }
+
+        public int CountDistinct()
+        {
+            if (string.IsNullOrWhiteSpace(_text))
+                _text = "MSLKFFPOKPOKAEMLE";
+
+            var distinctCharactersCount = _text.GroupBy(x => x).Select(x => new
+            {
+                character = x.Key,
+                count = x.Count()
+            });
+
+            var charactersCount = distinctCharactersCount.ToList();
+            foreach (var it in charactersCount)
+            {
+                Console.WriteLine($"{it.character} count = {it.count}");
+            }
+
+            return charactersCount.Count;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
